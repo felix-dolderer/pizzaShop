f563c3e37fab75c66572d8748217619dec774494
Felix Dolderer <felix.dolderer@rtskg.de>
2021-11-30 14:06:49 +0100

    fix paypal integration

--- worker/automations.js
+++ worker/automations.js
@@ -1,6 +1,7 @@
 const { Client, logger } = require('camunda-external-task-client-js');
 const { Variables } = require("camunda-external-task-client-js");
 const axios = require('axios');
+const nodemailer = require('nodemailer');
 const qs = require('qs');
 
 require('dotenv').config();
@@ -16,126 +17,131 @@ const client = new Client(config);
 
 // susbscribe to the topic: 'payment-validation'
 client.subscribe('payment-validation', async ({ task, taskService }) => {
-    console.log('Found payment-validation task');
-    validation(task, taskService);
+  console.log('Found payment-validation task');
+  validation(task, taskService);
 });
 
 // subscribt to the topic: 'invalid-payment'
 client.subscribe('invalid-payment', async ({ task, taskService }) => {
-    console.log('Found invalid-payment task');
-    sendMail(
-        task,
-        taskService,
-        'Bestellung fehlgeschlagen',
-        'Die Bestellung kann leider nicht durchgeführt werden, da die Zahlung nicht bestätigt werden konnte.'
-    );
+  console.log('Found invalid-payment task');
+  sendMail(
+    task,
+    taskService,
+    'Bestellung fehlgeschlagen',
+    'Die Bestellung kann leider nicht durchgeführt werden, da die Zahlung nicht bestätigt werden konnte.'
+  );
 });
 
 // subscribt to the topic: 'order-confirmation'
 client.subscribe('order-confirmation', async ({ task, taskService }) => {
-    console.log('Found order-confirmation task');
-    sendMail(
-        task,
-        taskService,
-        'Bestellung erfolgreich',
-        'Die Bestellung wurde erfolgreich empfangen und die Zahlung konnte bestätigt werden.'
-    );
+  console.log('Found order-confirmation task');
+  sendMail(
+    task,
+    taskService,
+    'Bestellung erfolgreich',
+    'Die Bestellung wurde erfolgreich empfangen und die Zahlung konnte bestätigt werden.'
+  );
 });
 
 // subscribt to the topic: 'inform-customer'
 client.subscribe('inform-customer', async ({ task, taskService }) => {
-    console.log('Found inform-customer task');
-    sendMail(
-        task,
-        taskService,
-        'Lieferung ist unterwegs',
-        `Deine Pizza wurde eben von ${task.variables.get('chef')} zubereitet und ist jetzt auf dem Weg zu Dir.`
-    );
+  console.log('Found inform-customer task');
+  sendMail(
+    task,
+    taskService,
+    'Lieferung ist unterwegs',
+    `Deine Pizza wurde eben von ${task.variables.get('chef')} zubereitet und ist jetzt auf dem Weg zu Dir.`
+  );
 });
 
 
 
 async function sendMail(task, taskService, mailSubject, mailText) {
-    var customerEmail = task.variables.get("email");
+  const customerEmail = task.variables.get("email");
 
-    var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: false,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PW
-        }
-    });
-    var mailOptions = {
-        from: process.env.MAIL_USER,
-        to: customerEmail,
-        subject: mailSubject,
-        text: mailText
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PW
     }
-    transporter.sendMail(mailOptions, function (err, info) {
-        if (err) {
-            console.log(err);
-        } else {
-            taskService.complete(task);
-            console.log('E-Mail sent. ' + info.response);
-        }
-    });
+  });
+  const mailOptions = {
+    from: process.env.MAIL_USER,
+    to: customerEmail,
+    subject: mailSubject,
+    text: mailText
+  }
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      taskService.complete(task);
+      console.log('E-Mail sent. ' + info.response);
+    }
+  });
 }
 
 async function validation(task, taskService) {
-    console.log("validation")
-    // Put your business logic here
+  console.log("validation")
+  // Put your business logic here
 
-    var pay_token = task.variables.get("paymentID");
-    var vars = new Variables()
+  const pay_token = task.variables.get("paymentID");
+  const vars = new Variables()
 
-    // var valid = pay_token === "test"
+  const data = qs.stringify({
+    'grant_type': 'client_credentials'
+  });
+  const config = {
+    method: 'post',
+    url: 'https://api-m.sandbox.paypal.com/v1/oauth2/token',
+    headers: {
+      'Authorization': 'Basic QVp4aDVjbklFZDluRm5oZ2xpa2Z3Y2QtNmdFUTFyaldFeWVVNW5hNi1mZVFCa2F6dF96NllQVFdhZmVtTkUzb1F4cDdWazUwRno4ZkNlZzQ6RU5KSWNUVjE0azJtMUEzNWdxWmR1NWJzdE1JUmdUS0pteTUzZVlEYmp3UkJRVF9MYjNjOTRuNGpWSEZnOUdDX2QxZ3R0QXRZaUVaVU1hdDM=',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Cookie': 'ts=vr%3D442b463916d0a4be405edbf2fff7fa80%26vreXpYrS%3D1663477526%26vteXpYrS%3D1568808549%26vt%3D442b466216d0a4be405edbf2fff7fa7e'
+    },
+    data,
+  };
 
-    var access_token = 'A21AAIOoLxiuw8-3izJELeEmA5Lmq8v_q8Va94ZAUHlRVaD8yvHBCHXLbqIm_kPB5e6DvFx6okEiwh1JCWvbXVt0f2tfr78BQ'
+  let access_token
+  const accessTokenResponse = await axios(config)
+  console.log(JSON.stringify(accessTokenResponse.data));
+  access_token = accessTokenResponse.data.access_token
 
-    var url = `https://api-m.sandbox.paypal.com/v2/checkout/orders/${pay_token}`
+  const url = `https://api-m.sandbox.paypal.com/v2/checkout/orders/${pay_token}`
 
-    var axios = require('axios')
-    await axios.get(url, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${access_token}`
-        }
-    })
-        .then(response => {
-            console.log(response.data)
+  const validationResponse = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${access_token}`
+    }
+  })
+  console.log(validationResponse.data)
+  const valid = validationResponse.data.status === 'COMPLETED'
+  vars.set('validPayment', valid)
 
-            var valid = response.data.status === 'COMPLETED'
-            vars.set('validPayment', valid)
+  taskService.complete(task, vars)
 
-            taskService.complete(task, vars)
-        })
-        .catch(err => {
-            console.error(err);
-        })
+  // vars.set('validPayment', valid)
 
+  // taskService.complete(task, vars)
 
-    // vars.set('validPayment', valid)
+  // var url = "https://api.brainblocks.io/api/session/" + pay_token + "/verify";
 
-    // taskService.complete(task, vars)
+  // await axios.get(url)
+  //     .then(response => {
+  //         console.log(response);
 
-    // var url = "https://api.brainblocks.io/api/session/" + pay_token + "/verify";
+  //         var vars = new Variables();
+  //         var valid = response.data.fulfilled;
+  //         vars.set('validPayment', valid);
 
-    // var axios = require('axios');
-    // await axios.get(url)
-    //     .then(response => {
-    //         console.log(response);
-
-    //         var vars = new Variables();
-    //         var valid = response.data.fulfilled;
-    //         vars.set('validPayment', valid);
-
-    //         taskService.complete(task, vars);
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
+  //         taskService.complete(task, vars);
+  //     })
+  //     .catch(error => {
+  //         console.log(error);
+  //     });
 
 }
